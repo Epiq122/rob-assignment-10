7b23d17de7597f019372856d43d7a46160e34c8b
Robert Gleason <l2ob122@gmail.com>
2022-11-06 16:27:51 -0800

    changed getmapping from days to do and weeks to week

--- src/main/java/com/coderscampus/Assignment10/web/MealsController.java
+++ src/main/java/com/coderscampus/Assignment10/web/MealsController.java
@@ -9,15 +9,29 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Random;
 
 @RestController
 public class MealsController {
-    @GetMapping("mealplanner/daily")
+    @GetMapping("mealplanner/day")
     public ResponseEntity<DailyResponse> getDailyMeals(String numCalories, String diet, String exclusions) {
         RestTemplate rt = new RestTemplate();
 
-        URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate").queryParam("apiKey"
-                , "7ee517e70bfa4b65b6c6f686cad6cc57").queryParam("timeFrame", "daily").build().toUri();
+        Random random = new Random();
+        int randomInt = random.nextInt((3000 - 2000) + 1) + 1000;
+
+
+        URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
+                                      .queryParam("timeFrame", "daily")
+                                      .queryParam("diet",
+                                              diet != null ? diet : "")
+                                      .queryParam("targetCalories", numCalories != null ? numCalories : randomInt)
+                                      .queryParam(
+                                              "exclude", exclusions != null ? exclusions : "")
+                                      .queryParam("apiKey"
+                                              , "7ee517e70bfa4b65b6c6f686cad6cc57")
+                                      .build()
+                                      .toUri();
 
 //        URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/recipes/716429/information").queryParam(
 //                "apiKey", "7ee517e70bfa4b65b6c6f686cad6cc57").queryParam("includeNutrition", "true").build().toUri();
@@ -27,13 +41,24 @@ public class MealsController {
         return response;
     }
 
-    @GetMapping("mealplanner/weekly")
+    @GetMapping("mealplanner/week")
     public ResponseEntity<WeeklyResponse> getWeeklyMeals(String numCalories, String diet, String exclusions) {
         RestTemplate rt = new RestTemplate();
+        Random random = new Random();
+        int randomInt = random.nextInt((3000 - 2000) + 1) + 1000;
 
-        URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate").queryParam("apiKey"
-                , "7ee517e70bfa4b65b6c6f686cad6cc57").queryParam("timeFrame", "week").queryParam("diet",
-                diet != null ? diet : "").build().toUri();
+
+        URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
+                                      .queryParam("timeFrame", "week")
+                                      .queryParam("diet",
+                                              diet != null ? diet : "")
+                                      .queryParam("targetCalories", numCalories != null ? numCalories : randomInt)
+                                      .queryParam(
+                                              "exclude", exclusions != null ? exclusions : "")
+                                      .queryParam("apiKey"
+                                              , "7ee517e70bfa4b65b6c6f686cad6cc57")
+                                      .build()
+                                      .toUri();
 
 //        URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/recipes/716429/information").queryParam(
 //                "apiKey", "7ee517e70bfa4b65b6c6f686cad6cc57").queryParam("includeNutrition", "true").build().toUri();
